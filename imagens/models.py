14a8712a4c92ea450fbf983d65b0a1921bf042b7
Laisa Muito Linda <laisa.rioverde@gmail.com>
2024-08-06 09:49:49 -0300

    serializer updated

--- imagens/models.py
+++ imagens/models.py
@@ -1,21 +1,33 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class OS(models.Model):
     os = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.os
+    
+class Setores(models.TextChoices):
+    DESMONTAGEM = "D", _("Desmontagem")
+    MONTAGEM = "M", _("Montagem")
 
 class Setor(models.Model):
     os = models.ForeignKey(OS, related_name='setores', on_delete=models.CASCADE)
-    nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=1, choices=Setores.choices)
 
     def __str__(self):
         return f"{self.os.os} - {self.nome}"
 
+class Etapas(models.TextChoices):
+    CHEGADA = "C", _("Chegada")
+    DESMONTADO = "D", _("Desmontado")
+    PRONTO = "P", _("Pronto")
+    MONTAGEM = "M", _("Montagem")
+    TESTE = "T", _("Teste")
+
 class Etapa(models.Model):
     setor = models.ForeignKey(Setor, related_name='etapas', on_delete=models.CASCADE)
-    nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=1, choices=Etapas.choices)
 
     def __str__(self):
         return f"{self.setor.nome} - {self.nome}"
--- imagens/serializers.py
+++ imagens/serializers.py
@@ -1,22 +1,26 @@
 from rest_framework import serializers
 from .models import OS, Setor, Etapa, Imagem
 
-class OSSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OS
-        fields = ['id', 'os']
-
-class SetorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Setor
-        fields = ['id', 'os', 'nome']
-
-class EtapaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Etapa
-        fields = ['id', 'setor', 'nome']
 
 class ImagemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Imagem
         fields = ['id', 'etapa', 'imagem']
+
+class EtapaSerializer(serializers.ModelSerializer):
+    imagens = ImagemSerializer(many=True)
+    class Meta:
+        model = Etapa
+        fields = ['id', 'setor', 'nome', 'imagens']
+
+class SetorSerializer(serializers.ModelSerializer):
+    etapas = EtapaSerializer(many=True)
+    class Meta:
+        model = Setor
+        fields = ['id', 'os', 'nome', 'etapas']
+
+class OSSerializer(serializers.ModelSerializer):
+    setores = SetorSerializer(many=True)
+    class Meta:
+        model = OS
+        fields = ['id', 'os', 'setores']
