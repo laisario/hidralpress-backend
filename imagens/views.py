3d85faa888f4a01ea022566f4f927585670ed930
Laisa Muito Linda <laisa.rioverde@gmail.com>
2024-10-31 14:44:15 -0300

    changes

--- imagens/views.py
+++ imagens/views.py
@@ -1,9 +1,14 @@
 import os
 from rest_framework import viewsets, response, views, status
 from django.core.files.storage import default_storage
-
+from django.core.cache import cache
+from rest_framework.decorators import action
 from .models import OS, Sector, Step, Image
 from .serializers import OSSerializerWrite, OSSerializerRead, SectorSerializer, StepSerializer, ImageSerializer, StepOsSerializer
+import subprocess
+import threading
+from rest_framework.response import Response
+
 
 class OSViewSet(viewsets.ModelViewSet):
     queryset = OS.objects.all()
@@ -19,6 +24,17 @@ class OSViewSet(viewsets.ModelViewSet):
         os = serializer.save(**serializer.validated_data)
         return response.Response(OSSerializerRead(os).data)
     
+    @action(detail=False, methods=['post'])
+    def execute_update_os(self, request):
+        cache.set("update_os_progress", "started")
+        threading.Thread(target=self.run_update_os).start()
+        return Response({"status": "ok"})
+
+    def run_update_os(self):
+        update_os = ["python", "manage.py", "update_os"]
+        process = subprocess.Popen(update_os, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        process.wait()
+
 
 class ValidateOSView(views.APIView):
     def post(self, request, format=None):
