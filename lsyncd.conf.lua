67d807056e9b61e398a76a962a8587db94153aed
Laisa Muito Linda <laisa.rioverde@gmail.com>
2024-11-05 01:10:52 -0300

    aa

--- lsyncd.conf.lua
+++ lsyncd.conf.lua
@@ -7,11 +7,12 @@ settings {
 sync {
     default.direct,
     source = "/mnt/c-prod",
-
     init = false,
-    delete = false,
+}
 
+bash = {
     onCreate = "bash notify_django.sh created ^sourcePathname",
     onDelete = "bash notify_django.sh deleted ^sourcePathname",
     onMove = "bash notify_django.sh renamed ^sourcePathname ^targetPathname"
+
 }
