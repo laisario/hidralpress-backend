7c581e0db2330e8064d694bceabc4a4e6880de8d
Laisa Muito Linda <laisa.rioverde@gmail.com>
2025-02-24 14:18:34 -0300

    aa

--- imagens/serializers.py
+++ imagens/serializers.py
@@ -83,14 +83,13 @@ class OSSerializerWrite(serializers.Serializer):
         elif video:
             video = Video.objects.create(step_os=step_os, video=video)
 
-        print(image, video, "aaaaaaaaaaaaaaaa")
         return {
             "id": image.id if image else video.id,
             "os": os.os,
             "sector": sector.name,
             "step": step.name,
-            "image": image.image.url if image else None,
-            "video": video.video.url if video else None
+            "image": ImageSerializer(image).data if image else None,
+            "video": VideoSerializer(video).data if video else None
         }
 
 class OSSerializerRead(serializers.ModelSerializer):
